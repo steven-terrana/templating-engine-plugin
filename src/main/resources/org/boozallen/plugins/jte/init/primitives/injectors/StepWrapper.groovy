2c6961e6cfbd52c08d4bb9ce02e21d2e72358e81
Carlos OKieffe <okieffe_carlos@bah.com>
2020-09-25 16:43:03 -0400

    reverted with block

--- src/main/resources/org/boozallen/plugins/jte/init/primitives/injectors/StepWrapper.groovy
+++ src/main/resources/org/boozallen/plugins/jte/init/primitives/injectors/StepWrapper.groovy
@@ -100,16 +100,15 @@ class StepWrapper extends TemplatePrimitive implements Serializable, Cloneable{
      *
      * @return An equivalent StepWrapper instance
      */
+    @SuppressWarnings("UnnecessaryObjectReferences")
     Object clone(){
         Object that = super.clone()
-        that.with {
-            name = this.name
-            library = this.library
-            sourceText = this.sourceText
-            sourceFile = this.sourceFile
-            config = this.config
-            injector = this.injector
-        }
+        that.name = this.name
+        that.library = this.library
+        that.sourceText = this.sourceText
+        that.sourceFile = this.sourceFile
+        that.config = this.config
+        that.injector = this.injector
         return that
     }
 
